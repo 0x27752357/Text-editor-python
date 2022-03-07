5c859e832dd6c9538c28ae582676ce159f005299
Amir <89149731+amirsbry@users.noreply.github.com>
2022-03-07 12:03:26 +0330

    Add files via upload

--- text_editor.py
+++ text_editor.py
@@ -1,9 +1,12 @@
 import os
-import string 
 from tkinter import *
 from tkinter import font
 from tkinter import colorchooser
-from turtle import right, window_height, window_width  
+from tkinter import filedialog
+from tkinter.filedialog import LoadFileDialog, SaveFileDialog, askopenfilename
+from tkinter.messagebox import showinfo
+
+from nbformat import write
 
 
 
@@ -11,7 +14,11 @@ from turtle import right, window_height, window_width
 
 def change_color() : 
     color = colorchooser.askcolor()
-    text_area.config(fg=color[1] , )
+    text_area.config(fg=color[1])
+    
+def back_color() : 
+    color = colorchooser.askcolor()
+    text_area.config(bg=color[1])
 
 def change_font(*args) :
     text_area.config(font=(font_name.get() , spin_button.get()))
@@ -21,22 +28,54 @@ def new_file() :
     text_area.delete(1.0 , END)
 
 def save_file() :
-    pass
-
+    file = filedialog.asksaveasfilename(initialfile="Untiteled.txt",
+                                    defaultextension=".txt",
+                                    filetypes=[("All Files" , "*.*"),
+                                               ("Text Documents" , "*.txt")])
+    if file is None :
+        return 
+    else :
+        try :
+            window.title(os.path.basename(file))
+        
+            file = open(file , "w")
+        
+            file.write(text_area.get(1.0 , END))
+        except Exception :
+            print("Can not save file ! ")
+        finally :
+            file.close()
+        
 def open_file() :
-    pass
-
+   file = askopenfilename(defaultextension=".txt",
+                          filetypes=[("All Files" , "*.*"),
+                                ("Text Documents" , "*.txt")])
+   try :
+       window.title(os.path.basename(file))
+       
+       text_area.delete(1.0 , END)
+       
+       
+       file = open(file , "r") 
+       
+       text_area.insert(1.0 , file.read())
+       
+   except Exception :
+       print("Can not open file ! ")
+   finally :
+       file.close()
+       
 def cut() :
-    pass
+    text_area.event_generate("<<Cut>>") 
 
 def copy() :
-    pass
+    text_area.event_generate("<<Copy>>") 
 
-def paste() :
-    pass
+def paste() :     
+    text_area.event_generate("<<Paste>>") 
 
 def about() :
-    pass
+    showinfo("This is made by me ! ") # i dont know what is wrong with this 
 
 def quit() :
     window.destroy()
@@ -78,8 +117,11 @@ text_area.config(yscrollcommand=scrol_bar.set)
 frame = Frame(window)
 frame.grid()
 
-color_button = Button(frame , text="color" , command=change_color)
+color_button = Button(frame , text="Color" , command=change_color)
 color_button.grid(row=0 , column=0 )
+
+back_button = Button(frame , text="Back Ground" , command=back_color )
+back_button.grid(row=0  , column=3)
 
 font_button = OptionMenu(frame , font_name , *font.families() , command=change_font)
 font_button.grid(row=0 , column=1)
@@ -91,12 +133,25 @@ menu_bar = Menu(window)
 window.config(menu=menu_bar)
  
 file_menu = Menu(menu_bar , tearoff=0)
-menu_bar.add_cascade( label="file" , menu=file_menu)
-file_menu.add_command(label="New"  , command=new_file)
+menu_bar.add_cascade( label="File"  , menu=file_menu)
+file_menu.add_command(label="New"   , command=new_file)
 file_menu.add_command(label="Open"  , command=open_file)
 file_menu.add_command(label="Save"  , command=save_file)
 file_menu.add_separator()
 file_menu.add_command(label="Exit"  , command=quit)
+
+
+
+edit_menu = Menu(menu_bar , tearoff=0)
+menu_bar.add_cascade(label="Edit"    , menu=edit_menu)
+edit_menu.add_command(label="Copy"   , command=copy)
+edit_menu.add_command(label="Paste"  , command=paste)
+edit_menu.add_command(label="Cut"    , command=cut)
+
+about_menu = Menu(menu_bar , tearoff=0)
+menu_bar.add_cascade(label="Help"   , menu=about_menu)
+about_menu.add_command(label="About", command=about)
+
 
 
 
